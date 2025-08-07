5a24755ec9929b84ace5019e9be9effd48c25dd1
Fouziya-1903 <118104880+Fouziya-1903@users.noreply.github.com>
2025-08-07 18:40:05 +0530

    hero section changes

--- script.js
+++ script.js
@@ -1,14 +1,18 @@
-const mainCup = document.getElementById("main-cup");
-const cupOptions = document.querySelectorAll(".cups img");
 
-cupOptions.forEach(cup =>{
-  cup.addEventListener("click", () =>{
+const mainCup = document.querySelector(".main-cup");
+
+
+const cupOptions = document.querySelectorAll(".cups ul li img");
+
+
+cupOptions.forEach(cup => {
+  cup.addEventListener("click", () => {
+    
     mainCup.style.opacity = 0;
 
-    setTimeout(() =>{
+    setTimeout(() => {
       mainCup.src = cup.src;
-
       mainCup.style.opacity = 1;
-    },300)
+    }, 300);
   });
 });
